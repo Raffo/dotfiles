92cc0e244c9f81ea58ada3e639b015ad28477884
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2021-09-16 09:47:11 +0000

    copy more files
    Signed-off-by: GitHub <noreply@github.com>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+# copy vscode settings
 mkdir -p $HOME/.config/Code/User
 cp vscode/settings.json $HOME/.config/Code/User/settings.json
+
+# copy .zshrc
+cp .zshrc $HOME/.zshrc
+
+# install nix
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
 
