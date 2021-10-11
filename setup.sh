5ea145d327f71d8a0dbd30bd4734940aa5965ef3
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2021-10-11 14:54:58 +0000

    omg
    Signed-off-by: GitHub <noreply@github.com>

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
 
 source /home/codespace/.nix-profile/etc/profile.d/nix.sh
 
-if [ -f "$CODESPACE_VSCODE_FOLDER/codespace.nix"]; then
+if [ -f "$CODESPACE_VSCODE_FOLDER/codespace.nix" ]; then
     nix-env -if $CODESPACE_VSCODE_FOLDER/codespace.nix
 fi
 
