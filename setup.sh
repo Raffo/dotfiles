f5a392701a654c4904f5077d6c73ff19cd85ab8c
Raffaele Di Fazio <difazio.raffaele@gmail.com>
2021-10-11 14:51:47 +0000

    auto nix-env
    Signed-off-by: GitHub <noreply@github.com>

--- setup.sh
+++ setup.sh
@@ -10,4 +10,10 @@ cp .zshrc $HOME/.zshrc
 # install nix
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
 
+source /home/codespace/.nix-profile/etc/profile.d/nix.sh
+
+if [ -f "$CODESPACE_VSCODE_FOLDER/codespace.nix"]; then
+    nix-env -if $CODESPACE_VSCODE_FOLDER/codespace.nix
+fi
+
 bash keepalive.sh &
